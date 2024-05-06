3be70d268b3868a442de0666111648ca720b50a5
simona <simona.musilova11@gmail.com>
2024-05-06 20:02:04 +0200

    rozsirene zadani

--- uvod_do_programovani_2/05_soubory/zapis_do_souboru/02_vytvoreni_souboru.py
+++ uvod_do_programovani_2/05_soubory/zapis_do_souboru/02_vytvoreni_souboru.py
@@ -5,3 +5,14 @@ obsah_souboru = input("Zadejte obsah souboru: ")
 
 with open(nazev_souboru, mode='w', encoding='utf-8') as output_file:
     print(obsah_souboru, file=output_file)
+
+
+# Rozsirene zadani - opakovane dotazovni na novy radek textu
+
+nazev_souboru = input("Zadejte nazev souboru: ")
+
+while True:
+    obsah_souboru = input("Zadejte obsah souboru: ")
+
+    with open(nazev_souboru, mode='a', encoding='utf-8') as output_file:
+        print(obsah_souboru, file=output_file)
